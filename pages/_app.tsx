c0d5f3e071bd7f3cf119de084bbca1060c93d63a
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-12-26 14:10:35 +0100

    Create login & register modals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/layout'
+import LoginModal from '@/components/Modals/LoginModal'
+import RegisterModal from '@/components/Modals/RegisterModal'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <RegisterModal />
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
