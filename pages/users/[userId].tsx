8f5f49de2da9d32f46dfd2980b4f5eaf641cdd75
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-12-29 18:37:20 +0100

    Create the profile user components

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -3,7 +3,8 @@ import { ClipLoader } from "react-spinners";
 
 import Header from "@/components/Header";
 import useUser from "@/hooks/useUser";
-import UserHero from "@/components/users/userHero";
+import UserHero from "@/components/users/UserHero";
+import UserBio from "@/components/users/UserBio";
 
 const UserView = () => {
     const router = useRouter();
@@ -21,7 +22,7 @@ const UserView = () => {
                     h-full
                 "
             >
-                <ClipLoader color="lightblue" size={80}/>
+                <ClipLoader color="lightblue" size={80} />
             </div>
         )
     }
@@ -29,7 +30,8 @@ const UserView = () => {
     return (
         <>
             <Header showBackArrow label={`@${fetchedUser?.username}`} />
-            <UserHero userId={userId} as string/>
+            <UserHero userId={userId as string} />
+            <UserBio userId={userId as string} />
         </>
     );
 }
