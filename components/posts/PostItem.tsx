d06fa7551d3b7e0e27aacf728de3ae2ad6be1d68
Oxooi <86341288+Oxooi@users.noreply.github.com>
2024-01-05 01:08:20 +0100

    change the hover elements

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -51,9 +51,7 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
   }, [data.createdAt])
 
   return (
-    <div
-      onClick={goToPost}
-      className="
+    <div onClick={goToPost} className="
         border-b-[1px] 
         border-neutral-800 
         p-5 
@@ -65,9 +63,7 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
         <Avatar userId={data.user.id} />
         <div>
           <div className="flex flex-row items-center gap-2">
-            <p
-              onClick={goToUser}
-              className="
+            <p onClick={goToUser} className="
                 text-white 
                 font-semibold 
                 cursor-pointer 
@@ -75,18 +71,18 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
             ">
               {data.user.name}
             </p>
-            <span
-              onClick={goToUser}
-              className="
+            <span onClick={goToUser} className="
                 text-neutral-500
                 cursor-pointer
-                hover:underline
                 hidden
                 md:block
             ">
               @{data.user.username}
             </span>
             <span className="text-neutral-500 text-sm">
+              ·
+            </span>
+            <span className="text-neutral-500 text-sm hover:underline">
               {createdAt}
             </span>
           </div>
@@ -101,8 +97,7 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 {data.comments?.length || 0}
               </p>
             </div>
-            <div
-              onClick={onLike}
+            <div onClick={onLike}
               className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500">
               <LikeIcon color={hasLiked ? 'red' : ''} size={20} />
               <p>
